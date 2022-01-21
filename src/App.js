c8ffce5b33aa257866bebd90584398d0e4180352
aburidho <ridhoabu127@gmail.com>
2022-01-21 08:41:32 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section className="hero">
+      <div className="navbar">
+
+        <div clasname="nav">
+        <div className="nav-item">
+    
+      </div>
     </div>
-  );
+  </div>
+    </section>
+  )
+
 }
 
 export default App;
